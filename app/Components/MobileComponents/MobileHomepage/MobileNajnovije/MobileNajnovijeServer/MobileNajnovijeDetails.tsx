605ac21a871fa285bde75cc74458006bec8da9c5
Vlado Simic <vladosimic525@gmail.com>
2025-02-17 12:20:08 +0100

    homepage images test2

--- app/Components/MobileComponents/MobileHomepage/MobileNajnovije/MobileNajnovijeServer/MobileNajnovijeDetails.tsx
+++ app/Components/MobileComponents/MobileHomepage/MobileNajnovije/MobileNajnovijeServer/MobileNajnovijeDetails.tsx
@@ -32,25 +32,41 @@ const MobileNajnovijeDetails = ({
   return (
     <Link href={`/${slug}`} style={{ textDecoration: "none" }}>
       <div style={{ marginBottom: "60px" }}>
-        <div className={mobileStyle.mobileSingleItem}>
+        <div
+          className={mobileStyle.mobileSingleItem}
+          style={{ position: "relative" }}
+        >
+          {index < 1 && (
+            <Image
+              style={{ position: "absolute", zIndex: "-1" }}
+              className={style.imageCover}
+              src={sourceUrl}
+              width={300}
+              height={200}
+              alt={title}
+              priority={index < 1 ? true : false}
+              fetchPriority={index < 1 ? "high" : "low"}
+            />
+          )}
           <Image
+            style={{ position: "relative", zIndex: "1" }}
             className={style.imageCover}
             src={sourceUrl}
             width={300}
             height={200}
             alt={title}
-            priority={index < 1 ? true : false}
-            fetchPriority={index < 1 ? "high" : "low"}
+            //priority={index < 1 ? true : false}
+            //fetchPriority={index < 1 ? "high" : "low"}
           />
           <div
             className={mobileStyle.mobileCategoryOverlay}
-            style={{ background: matchColors(theCategoryColor) }}
+            style={{ background: matchColors(theCategoryColor), zIndex: "10" }}
           >
             <span>{theCategoryColor}</span>
           </div>
           <div
             className={mobileStyle.mobileCommentCount}
-            style={{ background: matchColors(theCategoryColor) }}
+            style={{ background: matchColors(theCategoryColor), zIndex: "10" }}
           >
             {hasImages && <i className="bi bi-camera"></i>}
             {hasVideo || (hasIframe && <i className="bi bi-youtube"></i>)}
--- app/functions/categoryColors.ts
+++ app/functions/categoryColors.ts
@@ -2,7 +2,7 @@ export const categoryStyles = [
   { cat: "sport", color: "green" },
   { cat: "galerije", color: "purple" },
   { cat: "grude-online", color: "darkmagenta" },
-  { cat: "lifestyle", color: "chocolate" },
+  { cat: "lifestyle", color: "#731b3c" },
   { cat: "politika", color: "red" },
   { cat: "crna-kronika", color: "darkslategray" },
   { cat: "gospodarstvo", color: "#105d68" },
@@ -12,6 +12,7 @@ export const categoryStyles = [
   { cat: "ostale-vijesti", color: "darkslateblue" },
   { cat: "promo", color: "royalblue" },
   { cat: "izdvojeno", color: "darkblue" },
+  { cat: "nekategorizirano", color: "black" },
 ];
 
 export const matchColors = (color: string) => {
