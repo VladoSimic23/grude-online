ec969a310a393b95555f99b9a38b898d92b173d4
Vlado Simic <vladosimic525@gmail.com>
2025-05-18 16:40:04 +0200

    carousel edit

---
+++ app/api/image-download/route.ts
@@ -0,0 +1,27 @@
+import { NextResponse } from "next/server";
+
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+  const src = searchParams.get("src");
+
+  if (!src) {
+    return new NextResponse("Image source missing", { status: 400 });
+  }
+
+  try {
+    const imageRes = await fetch(src);
+    const contentType = imageRes.headers.get("content-type") || "image/jpeg";
+    const buffer = await imageRes.arrayBuffer();
+
+    return new NextResponse(Buffer.from(buffer), {
+      status: 200,
+      headers: {
+        "Content-Type": contentType,
+        "Content-Disposition": `attachment; filename="${src.split("/").pop()}"`,
+      },
+    });
+    // eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/no-unused-vars
+  } catch (error) {
+    return new NextResponse("Failed to fetch image", { status: 500 });
+  }
+}
