b69416078956749ceb248c634530ec2e64aeb570
Vlado Simic <vladosimic525@gmail.com>
2025-05-18 16:57:23 +0200

    awsd

--- app/api/image-download/route.ts
+++ app/api/image-download/route.ts
@@ -1,26 +1,26 @@
-// import { NextResponse } from "next/server";
+import { NextResponse } from "next/server";
 
-// export async function GET(request: Request) {
-//   const { searchParams } = new URL(request.url);
-//   const src = searchParams.get("src");
+export async function GET(request: Request) {
+  const { searchParams } = new URL(request.url);
+  const src = searchParams.get("src");
 
-//   if (!src) {
-//     return new NextResponse("Image source missing", { status: 400 });
-//   }
+  if (!src) {
+    return new NextResponse("Image source missing", { status: 400 });
+  }
 
-//   try {
-//     const imageRes = await fetch(src);
-//     const contentType = imageRes.headers.get("content-type") || "image/jpeg";
-//     const buffer = await imageRes.arrayBuffer();
+  try {
+    const imageRes = await fetch(src);
+    const contentType = imageRes.headers.get("content-type") || "image/jpeg";
+    const buffer = await imageRes.arrayBuffer();
 
-//     return new NextResponse(Buffer.from(buffer), {
-//       status: 200,
-//       headers: {
-//         "Content-Type": contentType,
-//         "Content-Disposition": `attachment; filename="${src.split("/").pop()}"`,
-//       },
-//     });
-//   } catch (error) {
-//     return new NextResponse(`Failed to fetch image ${error}`, { status: 500 });
-//   }
-// }
+    return new NextResponse(Buffer.from(buffer), {
+      status: 200,
+      headers: {
+        "Content-Type": contentType,
+        "Content-Disposition": `attachment; filename="${src.split("/").pop()}"`,
+      },
+    });
+  } catch (error) {
+    return new NextResponse(`Failed to fetch image ${error}`, { status: 500 });
+  }
+}
