fa7ae389d4d8f831d749f63c76b843cf9ccf9f59
Vlado Simic <vladosimic525@gmail.com>
2025-05-25 16:47:53 +0200

    awdd

--- app/Components/GoogleAds/GoogleAds.tsx
+++ app/Components/GoogleAds/GoogleAds.tsx
@@ -8,30 +8,35 @@ declare global {
     googletag: typeof googletag;
   }
 }
-export default function GoogleAds() {
-  // Ne trebaš declare global ako koristiš @types/googletag
-  // TypeScript već zna za window.googletag
 
+export default function GoogleAds() {
   useEffect(() => {
+    // Inicijaliziraj googletag ako nije definiran
     window.googletag = window.googletag || { cmd: [] };
 
     window.googletag.cmd.push(() => {
-      const slot = window.googletag
+      // Definiraj oglasni slot i dodaj servis
+      window.googletag
         .defineSlot("/31722200/desktop_970", [970, 250], "div-gpt-ad-1")
         ?.addService(window.googletag.pubads());
 
+      // Konfiguriraj oglase
       window.googletag.pubads().enableSingleRequest();
       window.googletag.enableServices();
+
+      // Prikaži oglas
       window.googletag.display("div-gpt-ad-1");
     });
   }, []);
 
   return (
     <>
+      {/* Učitaj Google Publisher Tag skriptu */}
       <Script
         src="https://securepubads.g.doubleclick.net/tag/js/gpt.js"
         strategy="afterInteractive"
       />
+      {/* Div za oglas */}
       <div id="div-gpt-ad-1" style={{ width: 970, height: 250 }} />
     </>
   );
